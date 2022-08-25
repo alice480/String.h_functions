40341c37e5a3b77418fc737fc38249af4241f6eb
alice480 <65951454+alice480@users.noreply.github.com>
2022-08-25 16:14:00 +0300

    Create s21_string.h

---
+++ s21_string.h
@@ -0,0 +1,10 @@
+#ifndef SRC_S21_STRING_H_
+#define SRC_S21_STRING_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+int s21_strlen(const char *str);
+int s21_strcmp(const char *str1, const char *str2);
+char* s21_strcpy(char *copy_str, const char *orig_str);
+
+#endif  //  SRC_S21_STRING_H_
